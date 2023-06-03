66c053b1ea179687c4584b3ad8d246ee1924e01c
Connor Littleton <littletonconnor@gmail.com>
2023-06-02 20:52:10 -0700

    Remove pm2 stuff and cleanup turbo

--- apps/www/ecosystem.config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  name: 'docs.connorlittleton',
-  exec_mode: 'cluster',
-  instances: 'max',
-  script: 'npm',
-  args: 'start',
-}
-
-module.exports = config
--- pm2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd apps/api
-pm2 start
-
-cd ../www
-pm2 start
